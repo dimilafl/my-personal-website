bf4d5fbabf39e6eaea78cafb369ed10ab3020638
Dimitri L <72954055+dimilafl@users.noreply.github.com>
2023-08-10 18:41:26 -0400

    styles

--- my_website/src/components/Skills.js
+++ my_website/src/components/Skills.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import '../styles.css';
 
 const Skills = () => (
   <section id="skills">
-    <h2>My Skills</h2>
-    <ul>
-      <li>JavaScript</li>
-      <li>Python</li>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>SQL</li>
-      <li>MongoDB</li>
-      <li>React.js</li>
-      {/* Add more skills */}
+    <h2 className="section-heading">My Skills</h2>
+    <ul className="skills-list">
+      <li className="skills-list-item">JavaScript</li>
+      <li className="skills-list-item">Python</li>
+      <li className="skills-list-item">HTML</li>
+      <li className="skills-list-item">CSS</li>
+      <li className="skills-list-item">SQL</li>
+      <li className="skills-list-item">MongoDB</li>
+      <li className="skills-list-item">React.js</li>
+      {/* ...other skills */}
     </ul>
   </section>
 );
